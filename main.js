c1b346a2745c659a4cbbcbdbd6c21b08c7c4f527
jonas <jonasalvesbueno@gmail.com>
2022-09-15 16:57:41 -0300

    Adicionando funcionalidade ao botão 'resolvido'

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 
 const submit = document.getElementById("submit");
 const storage = window.localStorage;
+d = new Date();
 
-const d = new Date();
 semana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
 ano = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
 // Aqui serão armazenados objetos com as infos das ordens de serviço
 let ordens = JSON.parse(storage.getItem("ordens")) || []
+let resolvidos = JSON.parse(storage.getItem("resolvidos")) || []
 
 function cleanField() {
     document.getElementById("lab").value = "";
@@ -20,12 +21,16 @@ function getData() {
     let lab = document.getElementById("lab").value
     let maq = document.getElementById("maq").value
     let prob = document.getElementById("prob").value
+    
+    // Criando objeto da Ordem de Serviço:
     let os = {
         "lab": lab,
         "maq": maq,
         "prob": prob,
-        "rep": dia
+        "rep": dia,
+        "stat": false
     };
+
     ordens.push(os)
     let tmp = JSON.stringify(ordens)
     storage.setItem("ordens", tmp)
@@ -43,6 +48,7 @@ function createOS() {
         let divOrdem = document.createElement("div");
         divOrdem.setAttribute("class", "ordem-servico")
         let ul = document.createElement("ul")
+        let osNumber = document.createElement("li")
         let lab = document.createElement("li")
         let maq = document.createElement("li")
         let prob = document.createElement("li")
@@ -50,27 +56,37 @@ function createOS() {
         let date = document.createElement("li")
     
         // Get data from localStorage:
+        osNumber.textContent = "Ordem de Serviço #"+osArr.indexOf(ordem)
+        osNumber.setAttribute("class", "titulo-os")
         lab.textContent = "Laboratório: "+ ordem.lab
         maq.textContent = "Máquina: "+ordem.maq
         prob.textContent = "Problema: "+ordem.prob
         date.textContent = "Reportado em: " + ordem.rep
-        status.textContent = "Status: pendente."
+        
+        status.textContent = ordem.stat ? "Status: Resolvido!" : "Status: Pendente."
     
         let resolvido = document.createElement("button")
         resolvido.innerText = "Resolvido!"
-    
+        resolvido.addEventListener("click", ()=>{
+            ordem.stat = !ordem.stat
+            divOrdem.classList.toggle("resolvido")
+            if (divOrdem.classList.contains("resolvido")) {
+                status.textContent = "Status: Resolvido!"
+            } else {
+                status.textContent = "Status: Pendente."
+            }
+        })
+        
+        ul.appendChild(osNumber)
         ul.appendChild(lab)
         ul.appendChild(maq)
         ul.appendChild(prob)
         ul.appendChild(date)
+        ul.appendChild(status)
         ul.appendChild(resolvido)
         divOrdem.appendChild(ul)
         divConteudo.appendChild(divOrdem)
-
     })
-
-
-
 }
 
 submit.addEventListener("click", getData)
