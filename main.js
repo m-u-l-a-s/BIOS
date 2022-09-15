fade9810ae4ed714752a7d9ed7dd01730948be94
Jonas <jonasalvesbueno@gmail.com>
2022-09-15 12:33:35 -0300

    Adicionando manipulação do localStorage com javascript

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
 const submit = document.getElementById("submit");
 const storage = window.localStorage;
+
 const d = new Date();
 semana = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
 ano = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-let ordens = []
+
+// Aqui serão armazenados objetos com as infos das ordens de serviço
+let ordens = JSON.parse(storage.getItem("ordens")) || []
 
 function cleanField() {
     document.getElementById("lab").value = "";
@@ -13,47 +16,58 @@ function cleanField() {
 }
 
 function getData() {
-    let lab = document.getElementById("lab")
-    let maq = document.getElementById("maq")
-    let prob = document.getElementById("prob")
-    ordens.push(lab.value, maq.value, prob.value)
-    storage.setItem("lab", lab.value)
-    storage.setItem("maq", maq.value)
-    storage.setItem("prob", prob.value)
-    console.log(ordens)
+    let dia = `${semana[d.getDay()]}, ${d.getDate()} de ${ano[d.getMonth()]} de ${d.getFullYear()} às ${d.getHours()} horas e ${d.getMinutes()} minutos.`
+    let lab = document.getElementById("lab").value
+    let maq = document.getElementById("maq").value
+    let prob = document.getElementById("prob").value
+    let os = {
+        "lab": lab,
+        "maq": maq,
+        "prob": prob,
+        "rep": dia
+    };
+    ordens.push(os)
+    let tmp = JSON.stringify(ordens)
+    storage.setItem("ordens", tmp)
     cleanField()
 }
 
 function createOS() {
-    let conteudo = document.getElementById("conteudo");
-    let os = document.createElement("div")
-    os.className = "ordem-servico"
-    let lab = document.createElement("p")
-    let maq = document.createElement("p")
-    let prob = document.createElement("p")
-    let status = document.createElement("p")
-    let date = document.createElement("p")
+    let conteudo = document.getElementById("ordem-servico");
+    let osArr = JSON.parse(storage.getItem("ordens"))
+    console.log(osArr)
 
-    let dia = `${semana[d.getDay()]}, ${d.getDate()} de ${ano[d.getMonth()]} de ${d.getFullYear()} às ${d.getHours()} horas e ${d.getMinutes()} minutos.`
-    let resolvido = document.createElement("button")
-    resolvido.innerText = "Resolvido!"
-    let excluir = document.createElement("button")
-    excluir.innerText = "Excluir"
+    // Create HTML elements:
+    osArr.forEach(ordem => {
+        
+        let ul = document.createElement("ul")
+        let lab = document.createElement("li")
+        let maq = document.createElement("li")
+        let prob = document.createElement("li")
+        let status = document.createElement("li")
+        let date = document.createElement("li")
+    
+        // Get data from localStorage:
+        lab.textContent = "Laboratório: "+ ordem.lab
+        maq.textContent = "Máquina: "+ordem.maq
+        prob.textContent = "Problema: "+ordem.prob
+        date.textContent = "Reportado em: " + ordem.rep
+        status.textContent = "Status: pendente."
+    
+        let resolvido = document.createElement("button")
+        resolvido.innerText = "Resolvido!"
+        let excluir = document.createElement("button")
+        excluir.innerText = "Excluir"
+    
+        ul.appendChild(lab)
+        ul.appendChild(maq)
+        ul.appendChild(prob)
+        ul.appendChild(date)
+        conteudo.appendChild(ul)
 
-    lab.textContent = "Laboratório: "+storage.getItem("lab")
-    maq.textContent = "Máquina: "+storage.getItem("maq")
-    prob.textContent = "Problema: "+storage.getItem("prob")
-    status.textContent = "Status: Pendente"
-    date.textContent = "Reportado em: "+dia
+    })
 
-    os.appendChild(lab)
-    os.appendChild(maq)
-    os.appendChild(prob)
-    os.appendChild(status)
-    os.appendChild(date)
-    os.appendChild(excluir)
-    os.appendChild(resolvido)
-    conteudo.appendChild(os)
+
 
 }
 
